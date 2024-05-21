949cd240df5a6b7aeb2f7d05037c495806dc3ded
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-05-21 18:19:19 +0000

    comprobar imagenes de pilotos

--- src/vistas/pilotos.js
+++ src/vistas/pilotos.js
@@ -8,7 +8,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/sainz.jpg" class="card-img-top" alt="Carlos Sainz">
+                <img src="../imagenes/sainz.jpg" class="card-img-top" alt="Carlos Sainz">
                 <div class="card-body">
                     <h5 class="card-title">Carlos Sainz</h5>
                     
@@ -18,7 +18,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/leclerc.jpg" class="card-img-top" alt="Charles Leclerc">
+                <img src="../imagenes/leclerc.jpg" class="card-img-top" alt="Charles Leclerc">
                 <div class="card-body">
                     <h5 class="card-title">Charles Leclerc</h5>
                     
@@ -28,7 +28,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/verstappen.jpg" class="card-img-top" alt="Max Verstappen">
+                <img src="../imagenes/verstappen.jpg" class="card-img-top" alt="Max Verstappen">
                 <div class="card-body">
                     <h5 class="card-title">Max Verstappen</h5>
                     
@@ -38,7 +38,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/checo.jpg" class="card-img-top" alt="Sergio Perez">
+                <img src="../imagenes/checo.jpg" class="card-img-top" alt="Sergio Perez">
                 <div class="card-body">
                     <h5 class="card-title">Sergio Perez</h5>
                     
@@ -53,7 +53,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/norris.jpg" class="card-img-top" alt="Lando Norris">
+                <img src="../imagenes/norris.jpg" class="card-img-top" alt="Lando Norris">
                 <div class="card-body">
                     <h5 class="card-title">Lando Norris</h5>
                     
@@ -63,7 +63,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/piastri.jpg" class="card-img-top" alt="Oscar Piastri">
+                <img src="../imagenes/piastri.jpg" class="card-img-top" alt="Oscar Piastri">
                 <div class="card-body">
                     <h5 class="card-title">Oscar Piastri</h5>
                     
@@ -73,7 +73,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/stroll.jpg" class="card-img-top" alt="Lance Stroll">
+                <img src="../imagenes/stroll.jpg" class="card-img-top" alt="Lance Stroll">
                 <div class="card-body">
                     <h5 class="card-title">Lance Stroll</h5>
                     
@@ -83,7 +83,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/nano.jpg" class="card-img-top" alt="Fernando Alonso">
+                <img src="../imagenes/nano.jpg" class="card-img-top" alt="Fernando Alonso">
                 <div class="card-body">
                     <h5 class="card-title">Fernando Alonso</h5>
                     
@@ -98,7 +98,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/yuki.jpg" class="card-img-top" alt="Yuki Tsunoda">
+                <img src="../imagenes/yuki.jpg" class="card-img-top" alt="Yuki Tsunoda">
                 <div class="card-body">
                     <h5 class="card-title">Yuki Tsunoda</h5>
                     
@@ -108,7 +108,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/daniel.jpg" class="card-img-top" alt="Daniel Ricciardo">
+                <img src="../imagenes/daniel.jpg" class="card-img-top" alt="Daniel Ricciardo">
                 <div class="card-body">
                     <h5 class="card-title">Daniel Ricciardo</h5>
                     
@@ -118,7 +118,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/hamilton.jpg" class="card-img-top" alt="Lewis Hamilton">
+                <img src="../imagenes/hamilton.jpg" class="card-img-top" alt="Lewis Hamilton">
                 <div class="card-body">
                     <h5 class="card-title">Lewis Hamilton</h5>
                     
@@ -128,7 +128,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="./imagenes/russell.jpg" class="card-img-top" alt="George Russell">
+                <img src="../imagenes/russell.jpg" class="card-img-top" alt="George Russell">
                 <div class="card-body">
                     <h5 class="card-title">George Russell</h5>
                     
