9ccabd9e0e5811771ae0c4f51f51dcb2ac86a98c
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2024-05-27 21:24:12 +0200

    cambio escuderias

--- src/vistas/escuderias.js
+++ src/vistas/escuderias.js
@@ -8,7 +8,7 @@ export default  {
             <!-- Columna 1 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/ferrari.jpg" class="card-img-top" alt="Ferrari">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/imagen-4-65cb58bbf1a82.jpg?crop=0.830xw:0.633xh;0.0779xw,0.235xh&resize=980:*" class="card-img-top" alt="Ferrari">
                     <div class="card-body">
                         <h5 class="card-title">Ferrari</h5>
                         
@@ -18,7 +18,7 @@ export default  {
             <!-- Columna 2 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/redbull.jpg" class="card-img-top" alt="Red Bull Racing">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/red-bull-rb20-12-65cf188317999.jpg?crop=0.844xw:0.665xh;0.0709xw,0.292xh&resize=980:*" class="card-img-top" alt="Red Bull Racing">
                     <div class="card-body">
                         <h5 class="card-title">Red Bull Racing</h5>
                         
@@ -33,7 +33,7 @@ export default  {
             <!-- Columna 1 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/mclaren.jpg" class="card-img-top" alt="McLaren">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/mclaren-mcl38-22-65cf3066cf204.jpg?crop=1.00xw:0.736xh;0,0.129xh&resize=980:*" class="card-img-top" alt="McLaren">
                     <div class="card-body">
                         <h5 class="card-title">McLaren</h5>
                         
@@ -43,7 +43,7 @@ export default  {
             <!-- Columna 2 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/astonmartin.jpeg" class="card-img-top" alt="Aston Martin">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/aston-martin-amr24-01-65cf2fe32bfb8.jpeg?crop=0.799xw:0.711xh;0.106xw,0.154xh&resize=980:*" class="card-img-top" alt="Aston Martin">
                     <div class="card-body">
                         <h5 class="card-title">Aston Martin</h5>
                         
@@ -58,7 +58,7 @@ export default  {
             <!-- Columna 1 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/visa.jpg" class="card-img-top" alt="Visa">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/si202402080602-65c5dcdbda108.jpg?crop=1.00xw:0.668xh;0,0.147xh&resize=980:*" class="card-img-top" alt="Visa">
                     <div class="card-body">
                         <h5 class="card-title">Visa</h5>
                         
@@ -68,7 +68,7 @@ export default  {
             <!-- Columna 2 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/mercedes.jpg" class="card-img-top" alt="Mercedes">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/mercedes-w15-05-65ccbdad146b1.jpg?crop=0.903xw:0.803xh;0.0657xw,0.126xh&resize=980:*" class="card-img-top" alt="Mercedes">
                     <div class="card-body">
                         <h5 class="card-title">Mercedes</h5>
                         
@@ -83,7 +83,7 @@ export default  {
             <!-- Columna 1 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/stake.jpg" class="card-img-top" alt="Stake">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/sauber-c44-02-65c1601516858.jpg?crop=0.837xw:0.745xh;0.0882xw,0.145xh&resize=980:*" class="card-img-top" alt="Stake">
                     <div class="card-body">
                         <h5 class="card-title">Stake</h5>
                         
@@ -93,7 +93,7 @@ export default  {
             <!-- Columna 2 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/alpine.jpg" class="card-img-top" alt="Alpine">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/alpine-a524-23-65c3ac6897bd2.jpg?crop=0.821xw:0.728xh;0.0887xw,0.136xh&resize=980:*" class="card-img-top" alt="Alpine">
                     <div class="card-body">
                         <h5 class="card-title">Alpine</h5>
                         
@@ -108,7 +108,7 @@ export default  {
             <!-- Columna 1 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/haas.jpg" class="card-img-top" alt="Haas">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/haas-vf-23-11-1675173869.jpg?crop=0.952xw:0.846xh;0.0294xw,0.0954xh&resize=980:*" class="card-img-top" alt="Haas">
                     <div class="card-body">
                         <h5 class="card-title">Haas</h5>
                         
@@ -118,7 +118,7 @@ export default  {
             <!-- Columna 2 -->
             <div class="col-md-6">
                 <div class="card mb-3">
-                    <img src="./imagenes/williams.jpg" class="card-img-top" alt="Williams">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/williams-fw42-04-65c0f1188cc4a.jpg?crop=0.747xw:0.560xh;0.130xw,0.264xh&resize=980:*" class="card-img-top" alt="Williams">
                     <div class="card-body">
                         <h5 class="card-title">Williams</h5>
                         
