e0840fff17e98c2f564f356d9f1bc5de123d8670
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2024-05-27 21:18:03 +0200

    cambio imagenes pilotos

--- src/vistas/pilotos.js
+++ src/vistas/pilotos.js
@@ -8,7 +8,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/sainz.jpg" class="card-img-top" alt="Carlos Sainz">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/sainz-65cf42b2dd3c6.jpg?crop=1.00xw:0.186xh;0,0.144xh&resize=980:*" class="card-img-top" alt="Carlos Sainz">
                 <div class="card-body">
                     <h5 class="card-title">Carlos Sainz</h5>
                     
@@ -18,7 +18,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/leclerc.jpg" class="card-img-top" alt="Charles Leclerc">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/leclerc2-65cf425e64e3e.jpg?crop=1.00xw:0.178xh;0,0.146xh&resize=980:*" class="card-img-top" alt="Charles Leclerc">
                 <div class="card-body">
                     <h5 class="card-title">Charles Leclerc</h5>
                     
@@ -28,7 +28,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/verstappen.jpg" class="card-img-top" alt="Max Verstappen">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/max-verstappen-01-65cf38df87c8a.jpg?crop=1.00xw:0.335xh;0,0.0681xh&resize=980:*" class="card-img-top" alt="Max Verstappen">
                 <div class="card-body">
                     <h5 class="card-title">Max Verstappen</h5>
                     
@@ -38,7 +38,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/checo.jpg" class="card-img-top" alt="Sergio Perez">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/sergio-perez-02-65cf395436557.jpg?crop=1.00xw:0.335xh;0,0.0935xh&resize=980:*" class="card-img-top" alt="Sergio Perez">
                 <div class="card-body">
                     <h5 class="card-title">Sergio Perez</h5>
                     
@@ -53,7 +53,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/norris.jpg" class="card-img-top" alt="Lando Norris">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/landa-norris-65cf4138b447a.jpg?crop=1.00xw:0.335xh;0,0.110xh&resize=980:*" class="card-img-top" alt="Lando Norris">
                 <div class="card-body">
                     <h5 class="card-title">Lando Norris</h5>
                     
@@ -63,7 +63,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/piastri.jpg" class="card-img-top" alt="Oscar Piastri">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/oscar-piastri-65cf4102306ec.jpg?crop=1.00xw:0.335xh;0,0.115xh&resize=980:*" class="card-img-top" alt="Oscar Piastri">
                 <div class="card-body">
                     <h5 class="card-title">Oscar Piastri</h5>
                     
@@ -73,7 +73,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/stroll.jpg" class="card-img-top" alt="Lance Stroll">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/lance-stroll-2-65cf3c6d769ac.jpg?crop=1.00xw:0.334xh;0,0.156xh&resize=980:*" class="card-img-top" alt="Lance Stroll">
                 <div class="card-body">
                     <h5 class="card-title">Lance Stroll</h5>
                     
@@ -83,7 +83,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/nano.jpg" class="card-img-top" alt="Fernando Alonso">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/fernando-alonso-3-65cf3c9deea67.jpg?crop=1.00xw:0.334xh;0,0.166xh&resize=980:*" class="card-img-top" alt="Fernando Alonso">
                 <div class="card-body">
                     <h5 class="card-title">Fernando Alonso</h5>
                     
@@ -98,7 +98,7 @@ export default  {
         <!-- Columna 1 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/yuki.jpg" class="card-img-top" alt="Yuki Tsunoda">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/yuki-tsunoda-65cf4d5df3b38.jpg?crop=1.00xw:0.335xh;0,0.0727xh&resize=980:*" class="card-img-top" alt="Yuki Tsunoda">
                 <div class="card-body">
                     <h5 class="card-title">Yuki Tsunoda</h5>
                     
@@ -108,7 +108,7 @@ export default  {
         <!-- Columna 2 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/daniel.jpg" class="card-img-top" alt="Daniel Ricciardo">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/daniel-ricciardo-65cf4d155d7df.jpg?crop=1.00xw:0.335xh;0,0.0647xh&resize=980:*" class="card-img-top" alt="Daniel Ricciardo">
                 <div class="card-body">
                     <h5 class="card-title">Daniel Ricciardo</h5>
                     
@@ -118,7 +118,7 @@ export default  {
         <!-- Columna 3 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/hamilton.jpg" class="card-img-top" alt="Lewis Hamilton">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/mercedes-w14-02-1676455886.jpg?crop=1.00xw:0.334xh;0,0.157xh&resize=980:*" class="card-img-top" alt="Lewis Hamilton">
                 <div class="card-body">
                     <h5 class="card-title">Lewis Hamilton</h5>
                     
@@ -128,7 +128,7 @@ export default  {
         <!-- Columna 4 -->
         <div class="col-md-3">
             <div class="card mb-3">
-                <img src="../imagenes/russell.jpg" class="card-img-top" alt="George Russell">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/mercedes-w14-07-1676455887.jpg?crop=1.00xw:0.334xh;0,0.101xh&resize=980:*" class="card-img-top" alt="George Russell">
                 <div class="card-body">
                     <h5 class="card-title">George Russell</h5>
                     
